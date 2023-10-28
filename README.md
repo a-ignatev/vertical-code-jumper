89be2555adf16f2c8f5d0115d1d9a1c8034732dc
Andrei Ignatev <andrei.ignatev@doctolib.com>
2023-10-28 11:52:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vertical Code Jumper
+
+ TODO
---
+++ media/main.js
@@ -0,0 +1,7 @@
+document.getElementById("hello").addEventListener("click", hello);
+
+function hello() {
+  const div = document.createElement("div");
+  div.innerHTML = "i'm going to jump!";
+  document.body.appendChild(div);
+}
