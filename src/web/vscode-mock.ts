4c4428fd5be4c8a02e7309c7694a537830fec0a8
Andrei Ignatev <andrei.ignatev@doctolib.com>
2023-11-02 20:19:46 +0100

    chore: add game controls for web

--- src/web/vscode-mock.ts
+++ src/web/vscode-mock.ts
@@ -1,11 +1,15 @@
 import { WebviewApi } from "vscode-webview";
 
-global.acquireVsCodeApi = <StateType = unknown>(): WebviewApi<StateType> => {
-  let state: any = undefined;
+const itemInStorage = localStorage.getItem("vertical-code-jumper.state");
+let state: any = (itemInStorage && JSON.parse(itemInStorage)) || {
+  music: initialMusicEnabled,
+};
 
+global.acquireVsCodeApi = <StateType = unknown>(): WebviewApi<StateType> => {
   return {
     setState: <T extends StateType | undefined>(newState: T): T => {
       state = newState;
+      localStorage.setItem("vertical-code-jumper.state", JSON.stringify(state));
 
       return state;
     },
@@ -22,3 +26,29 @@ global.acquireVsCodeApi = <StateType = unknown>(): WebviewApi<StateType> => {
     },
   };
 };
+
+const musicButton = document.getElementById("musicSwitcher");
+
+if (musicButton) {
+  musicButton.innerHTML = state.music ? "Music: ON" : "Music: OFF";
+
+  musicButton.addEventListener("click", () => {
+    const state = acquireVsCodeApi<{ music: boolean }>().getState();
+    musicButton.innerHTML = !state?.music ? "Music: ON" : "Music: OFF";
+
+    window.postMessage({
+      type: "setMusicEnabled",
+      enabled: !state?.music,
+    });
+  });
+}
+
+const restartButton = document.getElementById("restartButton");
+
+if (restartButton) {
+  restartButton.addEventListener("click", () => {
+    window.postMessage({
+      type: "restartGame",
+    });
+  });
+}
